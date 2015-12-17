bcbf6ac75ae1452e9cf3746a72b311bc7b60707c
Noel Loughran <nl@projectfusion.com>
2015-12-17 11:41:04 +0000

    Added more master code

--- src/main.c
+++ src/main.c
@@ -5,6 +5,12 @@ int main() {
   printf("Hello World\n");
   printf("This is another line from master\n");
   printf("Yet another line from master\n");
+  printf("Yet another line from master\n");
+  printf("Yet another line from master\n");
+  printf("Yet another line from master\n");
+  printf("Yet another line from master\n");
+  printf("Yet another line from master\n");
+  printf("Yet another line from master\n");
 
 
   return 1;
